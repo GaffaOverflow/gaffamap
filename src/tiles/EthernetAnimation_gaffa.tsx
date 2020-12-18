186b6e8994efdce87a556dc0a6f3ef9338ccf360
MakeNModify <the.xack@gmail.com>
2020-12-18 22:30:48 +0100

    Added Ethernet Animation for Portal

---
+++ src/tiles/EthernetAnimation_gaffa.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="EthernetAnimation_gaffa" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="EthernetAnimation_gaffa.png" width="96" height="96"/>
+ <tile id="8">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="8" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
