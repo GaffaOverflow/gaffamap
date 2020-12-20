268fa4cf8126efd04d524e3f9140fb071f60d709
StirlingDe <huglewis@gmx.de>
2020-12-20 02:18:17 +0100

    Lobby; Added new Capacitor and Collision; Updated Entrance with LGBT-Flag

--- src/tiles/EthernetAnimation_gaffa.tsx
+++ src/tiles/EthernetAnimation_gaffa.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.10.30" name="EthernetAnimation_gaffa" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+<tileset version="1.4" tiledversion="1.4.3" name="EthernetAnimation_gaffa" tilewidth="32" tileheight="32" tilecount="9" columns="3">
  <image source="EthernetAnimation_gaffa.png" width="96" height="96"/>
  <tile id="8">
   <animation>
@@ -8,6 +8,9 @@
    <frame tileid="2" duration="200"/>
    <frame tileid="5" duration="200"/>
    <frame tileid="8" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
   </animation>
  </tile>
 </tileset>
